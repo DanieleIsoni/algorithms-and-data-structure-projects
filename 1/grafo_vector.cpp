bc71674f09b5bf19460571e37f0ec6329a67fa68
Massimo Girondi <massimo@girondi.net>
2016-11-10 14:17:00 +0100

    base

---
+++ 1/grafo_vector.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+vector<vector<int> > adj;
+
+int main(){
+  ifstream in("input.txt");
+  int N,M;
+  in>>N>>M;
+  adj.resize(N);
+  for(int i=0;i<M;i++){
+    int from, to;
+    in>>from>>to;
+    adj[from].push_back(to);
+  }
+
+  for(int i=0;i<N;i++){
+    cout<<"Nodo "<<i<<" ha "<<adj[i].size()<<" vicini \n";
+    for(int j=0;j<adj[i].size();j++)
+      cout<<"  "<<adj[i][j]<<"\n";
+  }
+  return 0;
+}
