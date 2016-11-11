fca4d7caec52f6dd43eb098b04ebc52ea2864d61
daniso96 <daniele.isoni@studenti.unitn.it>
2016-11-11 18:59:01 +0100

    cicli separati con bug

--- 1/pokemon.cpp
+++ 1/pokemon.cpp
@@ -1,46 +1,28 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <stack>
-#include <set>
-#include <queue>
 
 using namespace std;
 
-vector<vector<int> > adj;
+vector<vector<int>> adj;
 
-void mostraPercorso(int u, int v, int * padri){
-  while (padri[u]!=padri[v]) {
-    cout<<padri[u]<<" "<<padri[v]<<endl;
-    if(padri[u]!=-1)
-      u=padri[u];
-    if(padri[v]!=-1)
-      v=padri[v];
+void stampaciclo(int s, int v, int * parents){
+  cout << "s " << s << " v " << v << endl;
+  while(s!=v){
+    cout << s << " ";
+    s = parents[s];
   }
+  cout << s << endl;
 }
 
-
-void bfs(int r, int N, int * padri){
-  queue<int> q;
-  q.push(r);
-  bool visitato[N];
-  for (int i = 0; i < N; i++) {
-    visitato[i]=false;
-  }
-  visitato[r]=true;
-
-  while (!q.empty()) {
-    int u = q.front();
-    q.pop();
-    for(int v : adj[u]){
-      if(!visitato[v]){
-        visitato[v]=true;
-        q.push(v);
-      }
-      else if(u<v){
-        cout<<"Trovato collegamento da "<<u<<" a "<<v<<endl;
-        mostraPercorso(u,v,padri);
-      }
+void dfs(int s, int * parents, bool * visited){
+  visited[s]=true;
+  for(int v: adj[s]){
+    if (!visited[v]){
+      parents[v]=s;
+      dfs(v, parents, visited);
+    } else if (s > v){
+      stampaciclo(s, v, parents);
     }
   }
 }
@@ -51,35 +33,36 @@ int main(){
   in>>N>>M;
   adj.resize(N);
 
-	//used to count cycles
-	int counterCycles=0;
-	int added[N];
-  for(int i=0;i<N;i++) added[i]=-1;
-
+  int parents[N];
+  for(int i=0;i<N;i++)
+    parents[i]=-1;
 
   for(int i=0;i<M;i++){
     int from, to;
     in>>from>>to;
-	  if(added[to]!=-1){
-			counterCycles++;
-      //trovato ciclo
-    }
-    else
-      added[to]=from;
     adj[from].push_back(to);
-  	adj[to].push_back(from);
+    adj[to].push_back(from);
   }
 
-  bfs(0,N,added);
+  bool visited[N];
+  for(int i=0; i<N; i++){
+    visited[i]=false;
+  }
 
+  dfs(0,parents, visited);
+  cout << endl;
+  for (int i = 0; i < N; i++) {
+    cout << parents[i] << " ";
+  }
+  cout << endl;
 
-	/*
+  /*
   for(int i=0;i<N;i++){
-    cout<<"Nodo "<<i<<" ha "<<adj[i].size()<<" vicini \n";
-    for(int j=0;j<adj[i].size();j++)
-      cout<<"  "<<adj[i][j]<<"\n";
-  }
-	*/
+  cout<<"Nodo "<<i<<" ha "<<adj[i].size()<<" vicini \n";
+  for(int j=0;j<adj[i].size();j++)
+  cout<<"  "<<adj[i][j]<<"\n";
+}
+*/
 
   return 0;
 }
