edcce18ceb35860a99ab0b7bc38cbff5dad304e7
Matteo Battilana <matbat96@gmail.com>
2016-11-11 14:42:33 +0100

    Added test cycles

--- 1/pokemon.cpp
+++ 1/pokemon.cpp
@@ -2,12 +2,27 @@
 #include <fstream>
 #include <vector>
 #include <stack>
+#include <set>
 
 using namespace std;
 
 vector<vector<int> > adj;
+//stack<stack<int>> cycles;
+set<int> supporto;
 
-
+void calcolaCiclo(int from, int to, int * added){
+  cout<<from<<" "<<to<<endl;
+  if(from!=-1 && to!=-1){
+    if(supporto.find(from)==supporto.end() )
+      supporto.insert(from);
+    if(supporto.find(to)==supporto.end() )
+      supporto.insert(to);
+    if(from != to )
+      calcolaCiclo(added[from],added[to],added);
+    else
+      cout<<"fine ciclo"<<endl;
+  }
+}
 
 int main(){
   ifstream in("input.txt");
@@ -17,21 +32,27 @@ int main(){
 
 	//used to count cycles
 	int counterCycles=0;
-	bool added[N];
-  for(int i=0;i<N;i++) added[i]=false;
+	int added[N];
+  for(int i=0;i<N;i++) added[i]=-1;
 
 
   for(int i=0;i<M;i++){
     int from, to;
     in>>from>>to;
-	  if(added[to])
+	  if(added[to]!=-1){
 			counterCycles++;
+      //trovato ciclo
+      calcolaCiclo(from,to, added);
+    }
+    else
+      added[to]=from;
     adj[from].push_back(to);
   	adj[to].push_back(from);
-		added[to]=true;
   }
 
 	cout<<"COUNTED --> "<<counterCycles<<endl;
+for(int i=0;i<N;i++) cout<<added[i]<<" ";
+
 
 	/*
   for(int i=0;i<N;i++){
