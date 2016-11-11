11b012e2df1f71c6af1cb881a0ffdeb20314463b
Matteo Battilana <matbat96@gmail.com>
2016-11-11 13:35:12 +0100

    "Added count cycles"

--- 1/pokemon.cpp
+++ 1/pokemon.cpp
@@ -1,49 +1,45 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <stack>
+
 using namespace std;
 
 vector<vector<int> > adj;
 
-int dfs(int s, bool * visitato){
-		visitato[s]=false;
-		int tot=0;
-		if((adj[s]).size()>1){
-			for(int vicino : adj[s]){
-				if(!visitato[vicino]){
-					tot+=dfs(vicino,visitato);
-				}
-				else
-					return 1;
-			}
-		}
-		
-		return tot;
-}
+
 
 int main(){
   ifstream in("input.txt");
   int N,M;
   in>>N>>M;
   adj.resize(N);
+
+	//used to count cycles
+	int counterCycles=0;
+	bool added[N];
+  for(int i=0;i<N;i++) added[i]=false;
+
+
   for(int i=0;i<M;i++){
     int from, to;
     in>>from>>to;
+	  if(added[to])
+			counterCycles++;
     adj[from].push_back(to);
-	adj[to].push_back(from);
+  	adj[to].push_back(from);
+		added[to]=true;
   }
-  
-  bool visitato[N];
-  for(int i=0;i<N;i++) visitato[i]=false;
-  
 
+	cout<<"COUNTED --> "<<counterCycles<<endl;
+
+	/*
   for(int i=0;i<N;i++){
     cout<<"Nodo "<<i<<" ha "<<adj[i].size()<<" vicini \n";
     for(int j=0;j<adj[i].size();j++)
       cout<<"  "<<adj[i][j]<<"\n";
   }
-  
-  
-  cout<<dfs(0,visitato);
+	*/
+
   return 0;
 }
