17ac3ad7e87844509d3a52b9ff471cab4c141c9c
Massimo Girondi <massimo@girondi.net>
2016-11-12 16:48:27 +0100

    Test solo la prima riga dell'output

--- 1/test1.sh
+++ 1/test1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 for i in $(seq 0 20); do
+  echo Test $i
   cp dataset/input/input$i.txt input.txt
-  time ./pokemon
+  ./pokemon >/dev/null
   diff -q <(head -n 1 output.txt) <(head -n 1 dataset/output/output$i.txt)
 done
