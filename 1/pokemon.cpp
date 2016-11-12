1e2a1bed38a36b482adcbcb3ece5229087798c1b
Matteo Battilana <matbat96@gmail.com>
2016-11-12 14:14:53 +0100

    Last matteo, manca solo disposizione e controllo intersezioni

--- 1/pokemon.cpp
+++ 1/pokemon.cpp
@@ -1,89 +1,82 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
 vector<vector<int>> adj;
-vector< vector<int> > cicli;
-int numCicli = 0;
+vector<vector<int>> cycles;
+int numeroCicli=0;
 int N,M;
-//variabile di debug
-int salvaCicloCounter = 0;
 
-void stampaCiclo(int s, int v, int * parents){
-  cout << "s " << s << " v " << v << endl;
-  while(s!=v){
-    cout << s << " ";
-    s = parents[s];
-  }
-  cout << s << endl;
-}
+int time=0;
 
-void salvaCiclo(int s, int v, int * parents){
-  //cout << "inizio salvaCiclo " << ++salvaCicloCounter <<"\n\n";
-  if(cicli[numCicli].empty()){
-    //cout << "resize\n\n";
-    cicli[numCicli].resize(0);
-  }
-  int i = 0;
-  while(s != v && s != -1){
-    //cout << "inserisco " << s << "\n\n";
-    cicli[numCicli].push_back(s);
-    s = parents[s];
-  }
-  cicli[numCicli].push_back(s);
-
-  /*
-    Ho messo <= a 2 perchè ho visto che o mi ritorna un ciclo o mi ritorna
-    una coppia sempre, quindi con questo controllo tolgo tutte le coppie
-  */
-  if(cicli[numCicli].size()<=2){
-    cicli[numCicli].clear();
-  } else if(s == -1 || cicli[numCicli].size()>2){
-    sort(cicli[numCicli].begin(), cicli[numCicli].end());
-    numCicli++;
-  }
-}
-
-void dfs(int s, int * parents, bool * visited){
-  //cout << "inizia dfs" << endl;
-  visited[s]=true;
-  for(int v: adj[s]){
-    if (!visited[v]){
-      parents[v]=s;
-      dfs(v, parents, visited);
-    } else if (s > v) {
-      //stampaCiclo(s, v, parents);
-      salvaCiclo(s, v, parents);
+void calcolaLung(int from, int to, int * padri){
+  cycles[numeroCicli].push_back(from);
+  int count=1;
+    while(from != to){
+      count++;
+      from=padri[from];
+      //cout<<" added "<<from<<endl;
+      cycles[numeroCicli].push_back(from);
     }
+  //  cout<<"cliclo n. "<<numeroCicli<<" ha "<<count<<" elementi "<<endl;
+    numeroCicli++;
+  }
+
+void dfs_visit(int u, int * color, int * padri){
+    color[u]=1;
+    for(int v : adj[u]){
+      if(color[v]==0){
+        padri[v]=u;
+        dfs_visit(v,color,padri);
+      }
+      if(color[v]==1 && v != padri[u])
+        calcolaLung(u,v,padri);
+    }
+    color[u]=2;
+  }
+
+void dfs(){
+  int color[N];
+  int d[N];
+  int f[N];
+  int padri[N];
+  for(int i=0; i<N; i++){
+    color[i]=0;
+    padri[i]=-1;
+  }
+
+  for(int i=0; i<N; i++){
+    if(color[i]==0)
+      dfs_visit(i,color,padri);
   }
 }
 
-int mcd(int a, int b) {
-    return b == 0 ? a : mcd(b, a % b);
-}
-
-int massimale(){
-  int MCD = cicli[0].size();
-  for (int i = 1; i < cicli.size(); i++) {
-    MCD = mcd(MCD, cicli[i].size());
-  }
-  return MCD;
+int MCD(int a, int b)
+{
+    do
+    {
+        if(a>b)
+        {
+            a = (a-b);
+        }
+        else if(a<b)
+        {
+            b = (b-a);
+        }
+    }
+    while(a!=b);
+    return a;
 }
 
 int main(){
   ifstream in("input.txt");
+//  int N,M;
   in>>N>>M;
   adj.resize(N);
-  cicli.resize(M);
-
-  int parents[N];
-  for(int i=0;i<N;i++)
-      parents[i]=-1;
-
-  /*for(int i=0;i<M;i++)
-    cicli[i].resize(M);*/
+  cycles.resize(N);
 
   for(int i=0;i<M;i++){
     int from, to;
@@ -92,35 +85,18 @@ int main(){
     adj[to].push_back(from);
   }
 
-  bool visited[N];
-  for(int i=0; i<N; i++){
-    visited[i]=false;
-  }
-  for (int i = 0; i < N; i++)
-    dfs(0,parents, visited);
+  dfs();
 
-  //cout << massimale() << endl;
-
-  /*cout << endl << "parents:" << " ";
-  for (int i = 0; i < N; i++) {
-    cout << parents[i] << " ";
-  }*/
-  cout << endl;
-  for (int i = 0; i < cicli.size(); i++){
-    for (int j = 0; j<cicli[i].size(); j++){
-      cout << cicli[i][j] << " ";
-    }
-    if(cicli[i].size() != 0)
-      cout << "\n\nlength: " << cicli[i].size() << "\n\n";
+  int MAX=cycles[0].size();
+  for(int i=1;i<numeroCicli;i++){
+    cout<<"Il ciclo "<<i<<" ha "<<cycles[i].size()<<" elementi \n";
+    MAX=MCD(MAX,cycles[i].size());
+    //for(int j=0;j<cycles[i].size();j++)
+    //  cout<<"  "<<cycles[i][j];
+    cout<<endl;
   }
+cout<<"POKEMON MASSIMI --> "<<MAX<<endl;
 
-/*
-  for(int i=0;i<N;i++){
-    cout<<"Nodo "<<i<<" ha "<<adj[i].size()<<" vicini \n";
-    for(int j=0;j<adj[i].size();j++)
-      cout<<"  "<<adj[i][j]<<"\n";
-  }
-*/
 
   return 0;
 }
