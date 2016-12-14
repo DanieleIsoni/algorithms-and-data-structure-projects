40bd75a32e0af2f02cdf03cab29bbffe90449389
Massimo Girondi <massimo@girondi.net>
2016-12-14 12:03:46 +0100

    tester più bello

--- 2/test.sh
+++ 2/test.sh
@@ -2,16 +2,18 @@ RED='\033[1;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-
 for i in $(seq 0 19); do
   printf "TEST $i\t"
   cp dataset/input/input$i.txt input.txt
-  #time
-   ./lich >/dev/null
-
+  #time ./lich >/dev/null
+  #TIME="$(bash -c "time ./lich > /dev/null" 2>&1)"
+  start=`date +%s%N`
+  ./lich >/dev/null
+  end=`date +%s%N`
+  TIME=`awk "BEGIN { print ($end - $start)/10000000/60 }" `
   if diff output.txt dataset/output/output$i.txt > /dev/null ; then
-    echo "------------> ${GREEN}OUTPUT CORRETTO${NC}"
+    echo "-->$TIME\t------> ${GREEN}OUTPUT CORRETTO${NC}"
   else
-    echo "------------> ${RED}OUTPUT SBAGLIATO${NC}"
+    echo "-->$TIME\t------> ${RED}OUTPUT SBAGLIATO${NC}"
 fi
 done
