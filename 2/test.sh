b6a439a071a260d16ddb518cf88a2659ff8de277
Massimo Girondi <massimo@girondi.net>
2016-12-14 11:25:00 +0100

    test

--- 2/test.sh
+++ 2/test.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
 for i in $(seq 0 20); do
   cp dataset/input/input$i.txt input.txt
   time ./lich >/dev/null
+  echo "testing file $i"
   diff -q output.txt dataset/output/output$i.txt
 done
