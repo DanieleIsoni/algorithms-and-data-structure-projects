5e045485fa7fa2c7cd6c3cdeadeadb1e7e4ac079
Massimo Girondi <massimo@girondi.net>
2016-12-14 11:37:30 +0100

    tester

--- 2/test.sh
+++ 2/test.sh
@@ -1,6 +1,12 @@
-for i in $(seq 0 20); do
+for i in $(seq 0 19); do
+  echo "TEST $i"
   cp dataset/input/input$i.txt input.txt
-  time ./lich >/dev/null
-  echo "testing file $i"
-  diff -q output.txt dataset/output/output$i.txt
+  #time
+   ./lich >/dev/null 
+
+  if diff output.txt dataset/output/output$i.txt > /dev/null ; then
+    echo "------------> OUTPUT $i CORRETTO"
+  else
+    echo "------------> OUTPUT $i SBAGLIATO"
+fi
 done
